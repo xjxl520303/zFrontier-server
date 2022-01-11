b3debc5e2812a50003c874b261a71183e4f4c747
xjxl520303 <xjxl520303@hotmail.com>
2022-01-11 15:10:51 +0800

    build: add heroku config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(2110);
+  app.enableCors();
+  await app.listen(process.env.PORT || 2110);
 }
 bootstrap();
