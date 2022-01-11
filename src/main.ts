b491461e2175b07866a9d43d87bf7edd7a7e58ae
xjxl520303 <xjxl520303@hotmail.com>
2022-01-11 16:13:30 +0800

    fix: try solve heroku m10 error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 2110);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
