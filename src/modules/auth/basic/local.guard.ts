fda7dcd4c4466bb19f91b52e9158fd94919fec47
xjxl520303 <xjxl520303@hotmail.com>
2022-02-09 12:05:36 +0800

    feat: add validation to body

--- src/modules/auth/basic/local.guard.ts
+++ src/modules/auth/basic/local.guard.ts
@@ -1,5 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  handleRequest(err, user, info, context, status) {
+    const request = context.switchToHttp().getRequest();
+    const { mobile, password } = request.body;
+    if (err || !user) {
+      if (!mobile) {
+        throw new HttpException({ message: '手机号不能为空' }, HttpStatus.OK);
+      } else if (!password) {
+        throw new HttpException({ message: '密码不能为空' }, HttpStatus.OK);
+      } else {
+        throw err || new UnauthorizedException();
+      }
+    }
+    return user;
+  }
+}
